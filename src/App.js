9b7f0c686abf0ad4723faf5497430ad497049aeb
Hayden D'souza <hayden2905@Haydens-MacBook-Air.local>
2021-12-16 07:49:37 -0800

    preliminaries

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Intro from './components/intro.js'
+const App = () => (
+  <div>
+    <Intro></Intro>
+  </div>
+)
+
+export default App
---
+++ src/components/intro.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './intro.css'
+
+const Intro = () => (
+  <div>
+  <div className='intro'>
+    <h1 style={{fontSize: '30px'}}>Hello! I'm Hayden D'Souza</h1>
+    </div>
+    <div className='description'>
+      <p>I am a sophomore studying Math Of Computation at UCLA. I'm still adding stuff to this website - stay tuned for updates :)</p>
+    </div>
+  </div>
+)
+
+export default Intro
+
